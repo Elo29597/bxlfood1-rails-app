e4f32ac9c553c295d11776cf460a2e28ac267873
Eloise de Streel <elo0097@hotmail.com>
2020-05-17 20:47:59 +0200

    adding model restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,17 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+
+   get "restaurants" => "restaurants#index"
+
+ get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
+     get "restaurants/new", to: "restaurants#new"
+     # The `new` route needs to be *before* `show` route.
+     post "restaurants", to: "restaurants#create"
+
+     get "restaurants/:id/edit", to: "restaurants#edit"
+     patch "restaurants/:id", to: "restaurants#update"
+      delete "restaurants/:id", to: "restaurants#destroy"
 end
 
