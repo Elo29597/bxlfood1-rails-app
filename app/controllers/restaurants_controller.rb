dd1734c8ec73a12fe83fcd3320c56ad61ffbd07f
Eloise de Streel <elo0097@hotmail.com>
2020-05-21 14:36:27 +0200

    régler des problèmes de frontend

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,63 +2,56 @@ class RestaurantsController < ApplicationController
 
    def index
     @restaurants = Restaurant.all
+
     if params[:query].present?
       @restaurants = Restaurant.where(address: params[:query])
     else
       @restaurants = Restaurant.all
-  end
-end
+    end
+    end
 
   def show
     @restaurant = Restaurant.find(params[:id])
   end
 
-   def new
+  def new
    @restaurant = Restaurant.new # needed to instantiate the form_for
   end
 
-   def create
-@restaurant = Restaurant.new(restaurant_params)
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
 
     # no need for app/views/restaurants/create.html.erb
     redirect_to restaurant_path(@restaurant)
   end
 
-
-
-   def edit
+  def edit
     @restaurant = Restaurant.find(params[:id])
-
 
   end
 
-   def update
-  @restaurant = Restaurant.find(params[:id])
+  def update
+    @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
-
     # no need for app/views/restaurants/update.html.erb
     redirect_to restaurant_path(@restaurant)
   end
 
 
-
-   def destroy
-     @restaurant = Restaurant.find(params[:id])
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
-
     # no need for app/views/restaurants/destroy.html.erb
-
-
     # no need for app/views/restaurants/destroy.html.erb
     redirect_to restaurants_path, :notice => "Your post has been deleted successfully."
   end
 end
+
 private
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :description, :photo)
-
 
   end
 
